51003db2874c0f11237c5b55267f5bcf06e736ee
Kseniya Vasileva <vasilieva.kss@gmail.com>
2024-04-23 12:42:27 +0400

    added city component

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+
+import { useCities } from "../../contexts/CitiesContext";
+
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -9,12 +12,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
+
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
